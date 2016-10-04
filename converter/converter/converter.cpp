ee9f0a2073c2a52fecd9f605d82dc2d9dea969f3
Ben <Ben@BABYGIRL-THE-SE>
2016-10-04 12:37:34 -0700

    added comments to the assignments

--- converter/converter/converter.cpp
+++ converter/converter/converter.cpp
@@ -1,13 +1,35 @@
+/*********************************************************************
+** Author:Benjamin Fields
+** Date:9/21/2016
+** Description: Program that contains two functions used to convert a 
+binary number into a decimal integer and a decimal integer into a its
+binary representation
+** Dependencies: need the string library and the cmath library as well
+as the iostream library for input and output
+*********************************************************************/
+
+
 #include <iostream>
 #include <string>
 #include <cmath>
 
+/*********************************************************************
+** Description: Binary to decimal. Function takes as a parameter a 
+c++ string and returns the decimal integer value. This function also 
+employs a recursive technique to solve the problem.
+**
+*********************************************************************/
 int binToDec(std::string myString)
 {
-	std::cout << "Entered Recursive function with string: " << myString.c_str() << std::endl;
+	//debug line to track program
+	//std::cout << "Entered Recursive function with string: " << myString.c_str() << std::endl;
+
+	//declare the base case
 	if (myString.length() == 1)
 	{
-		std::cout << "Base Case reached" << std::endl;
+		//std::cout << "Base Case reached" << std::endl;//debug line 
+		
+		//check if the leftmost character is a 1 or 0 to add the proper value
 		if (myString[0] == '1')
 		{
 			return (int)pow(2, 0);
@@ -19,44 +41,79 @@ int binToDec(std::string myString)
 	}
 	else
 	{
+		//recursive step
 		if (myString[0] == '1')
 		{
+			//if left most character is one add 2^power of correct power based on string
+			//and concatenate that with the value returned from the recursive call with 
+			//the first character removed
 			return (int)pow(2, myString.length() - 1) + binToDec(myString.erase(0, 1));
 		}
 		else
 		{
+			//if 0 then dont add anything but recursively call for next positions
+			//with the first character removed
 			return 0 + binToDec(myString.erase(0, 1));
 		}
 	}
 }
 
+/*********************************************************************
+** Description:decimal to binary. Takes as a parameter one integer 
+and returns the binary C++ string representation. The function also 
+uses a recursive technique to solve the problem.
+**
+*********************************************************************/
 std::string decToBin(int num) 
 {
-	std::cout << "Entered decToBin Recursive Function with number: "<<num << "(base case value) and remainer: "<<num%2<< std::endl;
+	//debug line for tracking
+	//std::cout << "Entered decToBin Recursive Function with number: "<<num << "(base case value) and remainer: "<<num%2<< std::endl;
+	
+	//check base case which is reached when the division by two returns 1 or 0 
+	//indicating the last position is reached
 	if (num == 0)
 	{
-		std::cout << "Base case reached" << std::endl;
+		//std::cout << "Base case reached" << std::endl;//debug line
 		return "0";
 	}
 	else if (num == 1)
 	{
-		std::cout << "Base case reached" << std::endl;
+		//std::cout << "Base case reached" << std::endl;//debug line
 		return "1";
 	}
 	else
 	{
+		//recursive step called 
 		return decToBin(num/2) + std::to_string(num%2);
 	}
 }
 
+
+/*********************************************************************
+** Description:Main function that is used to drive the testing of the 
+two convert functions listed above. Tests a conversion of an integer 
+to binary and a binary number to integer.
+**
+*********************************************************************/
 int main(int argc, char* argv[])
 {
+	//create test string
 	std::string binary = "10110111";
+
+	//create test int
 	int number = 183;
+
+	//test the binary to decimal conversion
 	int convertedBinary = binToDec(binary);
+
+	//print to check the value returned
 	std::cout << "\n\nBack in main after conversion of binary number to integer" << std::endl;
 	std::cout << "Value should equal 183. Value returned: "<<convertedBinary << "\n\n"<<std::endl;
+
+	//test decimal to binary
 	std::string convertedInteger = decToBin(number);
+
+	//print to check value returned
 	std::cout << "\n\nBack in main after conversion of integer to binary" << std::endl;
 	std::cout << "String should be 10110111. Returned string: "<<convertedInteger <<"\n\n"<< std::endl;
 	std::cout << "Test 1: " << binToDec("1010") << std::endl;
--- stringSort/stringSort/stringSort.cpp
+++ stringSort/stringSort/stringSort.cpp
@@ -5,25 +5,39 @@
 ** algorithm with an array of C++ strings (std::string). The function
 ** should take two parameters: 1-array of strings(pointer) 2-size of 
 ** array. Function should be case insensitive
-** Dependencies: 
+** Dependencies: need the cctype library and the string library
 *********************************************************************/
 
 #include<iostream>
 #include <string>
 #include <cctype>
 
+
+/*********************************************************************
+** Description:function takes two c++ strings and compares them with 
+a case insensitive method. The function returns an integer value based
+on the comparison results of the two strings
+**
+*********************************************************************/
 //returns 1 if str1 < str2, 0 if equal and -1 if str1 > str2
 int caseInsensitiveStringCompare(const std::string &str1, const std::string &str2)
 {
+	//create c strings to hold the c++ string for comparision 
 	char cstr1[255];
 	char cstr2[255];
+
+	//prevent a buffer overflow
 	if (str1.length() > 254 || str2.length() > 254)
 	{
 		std::cout << "Buffer overflow error" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+
+	//copy the c++ strings to the c strings
 	strcpy(cstr1, str1.c_str());
 	strcpy(cstr2, str2.c_str());
+
+	//convert each c string to all uppercase
 	for (int i = 0; i < (int)strlen(cstr1); i++)
 	{
 		cstr1[i] = toupper(cstr1[i]);
@@ -32,33 +46,57 @@ int caseInsensitiveStringCompare(const std::string &str1, const std::string &str
 	{
 		cstr2[i] = toupper(cstr2[i]);
 	}
+
 	return strcmp(cstr1, cstr2);
 }
 
+/*********************************************************************
+** Description: This function is an implementation of the selection 
+sort algorithm as presented in the class textbook. This algorithm is
+modified to sort c++ strings. It takes two parameters which are the 
+array to sort and the size of the array or number of strings in this 
+case.
+**
+*********************************************************************/
 void stringSort(std::string array[], int numStrs)
 {
+	//assign values used to track progress of algorithm
 	int startScan, minIndex;
+
 	std::string minValue;
+
+	//begin scanning array
 	for (startScan = 0; startScan < (numStrs - 1); startScan++)
 	{
 		minIndex = startScan;
 		minValue = array[startScan];
+		//increment through positions to compare values
 		for (int index = startScan + 1; index < numStrs; index++)
 		{
+			//caompare using custom string comparision function
 			if (caseInsensitiveStringCompare(array[index], minValue) < 0)
 			{
-				//std::cout << "Applying switch" << std::endl;
+				//std::cout << "Applying switch" << std::endl;//debug line
+
+				//apply swap 
 				minValue = array[index];
 				minIndex = index;
 			}
 		}
+		//update variables to reflect latest progress of algorithm
 		array[minIndex] = array[startScan];
 		array[startScan] = minValue;
 	}
 }
 
+/*********************************************************************
+** Description:basic function implemented to provide an easy method 
+for printing the array when running checks
+**
+*********************************************************************/
 void printArray(std::string array[], int size)
 {
+	//loop through each position and print to screen
 	for (int i = 0; i < 8; i++)
 	{
 		std::cout << *(array + i) << " | ";
@@ -66,15 +104,28 @@ void printArray(std::string array[], int size)
 	std::cout << "\n";
 }
 
+
+/*********************************************************************
+** Description:Main function used to drive the testing of the string
+sort function. This function is the entry point of the program and 
+tests a predetermined array of strings
+**
+*********************************************************************/
 int main(int argc, char* argv[])
 {
-	std::cout << "\n\nCheckpoint 1\n\n" << std::endl;
+	//std::cout << "\n\nCheckpoint 1\n\n" << std::endl;//debug line
+
+	//create test array
 	std::string testString[8] = { "ben","Joe","adam","Dog","CAT","YOU","XAVIER","JOHN" };
 	printArray(testString, 8);
-	std::cout << "\n\nCheckpoint 2\n\n" << std::endl;
+	//std::cout << "\n\nCheckpoint 2\n\n" << std::endl;//debug line
+
+	//test the sort function
 	stringSort(testString, 8);
-	std::cout << "\n\nCheckpoint 3\n\n" << std::endl;
-	std::cout << "Back in main after string sort" << std::endl;
+
+	//std::cout << "\n\nCheckpoint 3\n\n" << std::endl;//debug line
+	//std::cout << "Back in main after string sort" << std::endl;//debug line
+
 	printArray(testString, 8);
 	system("PAUSE");
 	return 0;
